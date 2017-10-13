133bca8dfba1d6e6a7168ee5c95750adaa075ece
Kamil S Jaroň <376090@mail.muni.cz>
2017-10-13 11:09:26 +0200

    springtail repetitions

---
+++ scripts/Fcan/process_repreats.R
@@ -0,0 +1,9 @@
+springtail_repeats <- read.table('data/Fcan/fcand_repeats.txt', header = T)
+
+TEs_lower <- springtail_repeats$Repeat %in% c('SINE', 'LINE', 'DNA', 'Retroposon')
+TEs_upper <- springtail_repeats$Repeat %in% c('SINE', 'LINE', 'DNA', 'Retroposon', 'Unknown')
+overall <- 0.233
+
+sum(springtail_repeats[TEs_lower,'Percentage']) * overall
+sum(springtail_repeats[TEs_upper,'Percentage']) * overall
+
---
+++ scripts/pull_data.sh
@@ -0,0 +1,1 @@
+# get data from NCBI
