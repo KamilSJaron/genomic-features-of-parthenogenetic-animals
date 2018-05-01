393b696ced85832e738faa9c8b5d8af3ec651607
Kamil S Jaroň <376090@mail.muni.cz>
2018-05-02 00:14:33 +0200

    fixed busco script (hopefully)

--- scripts/busco.sh
+++ scripts/busco.sh
@@ -1,14 +1,16 @@
 #/bin/bash
 
 # this part is required only for Vital-it (if you have running BUSCO, delete it)
-export PATH=\$PATH:/scratch/beegfs/monthly/ptranvan/Software/busco/2.0/
-module add Blast/ncbi-blast/2.2.31+
 module add SequenceAnalysis/HMM-Profile/hmmer/3.1b2
-module add SequenceAnalysis/GenePrediction/augustus/3.2.2
-export AUGUSTUS_CONFIG_PATH=/scratch/beegfs/monthly/kjaron/augustus_config
+module add SequenceAnalysis/GenePrediction/augustus/3.2.3
+export AUGUSTUS_CONFIG_PATH=/home/kjaron/src/busco-master/augustus_config
 
 # run busco
-# 1 - genome
+# 1 - gzipped genome
 # 2 - database with metazons
 # 3 - output directory
-BUSCO.py -i $1 -o $3 -m geno -l $2 -c 32
+zcat $1 > genome.fa
+
+run_BUSCO.py -i genome.fa -o $3 -m geno -l $2 -c 32
+
+rm genome.fa
