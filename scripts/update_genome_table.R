4246577fa17192137e9917ad3736fb453c2caf3d
Kamil S Jaroň <376090@mail.muni.cz>
2018-05-08 18:32:26 +0200

    update gebone table takes genomescope results

--- scripts/update_genome_table.R
+++ scripts/update_genome_table.R
@@ -1,6 +1,13 @@
+#################
+### load data ###
+#################
+
+# table to update
 tab_file <- 'tables/genome_table.tsv'
 genome_tab <- read.table(tab_file, header = T, stringsAsFactors = F, skip = 1)
-colnames(genome_tab)[3:5] <- c('assembly_size[M]', 'number_of_scaffolds[k]', 'N50[k]')
+# following line corrects names that are automatically replaced when R loads the table
+colnames(genome_tab)[c(3:5, 13)] <- c('assembly_size[M]', 'number_of_scaffolds[k]', 'N50[k]', 'haploid_length[M]')
+# download table for cases if there is a new organism
 dl_table <- read.table('tables/download_table.tsv', header = T, row.names = 1, stringsAsFactors = F)
 
 #####################################################
@@ -8,7 +15,8 @@ dl_table <- read.table('tables/download_table.tsv', header = T, row.names = 1, s
 #	assembly_size number_of_scaffolds N50           #
 #####################################################
 
-stat_files <- list.files(path = ".", pattern = "genome.stats", recursive = T, include.dirs = T)
+stat_files <- list.files(path = "data", pattern = "genome.stats", recursive = T, include.dirs = T)
+stat_files <- paste('data', genomescope_files, sep = '/')
 
 expand_table_if_needed <- function(.sp, .genome_tab){
 	row <- .sp == .genome_tab$code
@@ -91,16 +99,72 @@ for(busco_file in busco_files){
 	genome_tab[row, c('complete', 'fragmented', 'duplicated', 'missing')] <- read_busco(busco_file)
 }
 
-# TODO
-
 ####################################
 # ATLAS ( scripts/est_theta.sh )   #
 # ML_heterozygosity                #
 ####################################
+
+# TODO
 
 #############################################################
 # GenomeScope ( scripts/GenomeScope.sh )                    #
 # kmer_genome_size, kmer_heterozygosity, kmer_repetitions   #
 #############################################################
 
-write.table(genome_tab, tab_file, quote = F, sep = '\t', row.names = F)
+parse_genomescope_summary <- function(file){
+    genoscope_file <- readLines(file)
+    line <- ssplit(genoscope_file[5], ' ')
+	het_string <- line[line != ''][3]
+    heterozygosity <- as.numeric(substr(het_string, 0, nchar(het_string) - 1))
+
+    line <- ssplit(genoscope_file[6], ' ')
+    haploid_genome <- as.numeric(gsub(",", "", line[line != ''][6]))
+
+    line <- ssplit(genoscope_file[7], ' ')
+    repeats <- (as.numeric(gsub(",", "", line[line != ''][6])) * 100) / haploid_genome
+
+	if( heterozygosity == -100 ){
+		return( c(NA, NA, NA) )
+	} else {
+		return( c(round(haploid_genome / 1e6, 1), round(repeats, 2), round(heterozygosity, 2)) )
+	}
+}
+
+genomescope_files <- list.files(path = "data", pattern = "summary.txt", recursive = T, include.dirs = T)
+genomescope_files <- paste('data', genomescope_files, sep = '/')
+
+for(genomescope_file in genomescope_files){
+	sp <- strsplit(genomescope_file, "/")[[1]][2]
+	genome_tab <- expand_table_if_needed(sp, genome_tab)
+	row <- sp == genome_tab$code
+
+	genome_tab[row, c('haploid_length[M]', 'repeats', 'heterozygosity')] <- parse_genomescope_summary(genomescope_file)
+}
+
+######################
+### Sort the table ###
+######################
+
+desired_order <- c('Pfor1',
+                   'Avag1', 'Aric1', 'Rmac1', 'Rmag1',
+				   'Lcla1', 'Cbir1', 'Fcan1',
+				   'Pvir1', 'Dpul1',
+				   'Pdav1', 'Dcor1', 'Dpac1', 'Minc1', 'Minc2', 'Minc3', 'Mjav1', 'Mjav2', 'Mare1', 'Mare2', 'Mflo1', 'Mflo2', 'Ment1',
+				   'Hduj1', 'Rvar1')
+if ( length(desired_order) == nrow(genome_tab) ){
+	row.names(genome_tab) <- genome_tab$code
+	genome_tab <- genome_tab[desired_order, ]
+} else {
+	cat('There are species in the table with unknown position by the hand-made-order in this script.\n')
+	cat('namely : ')
+	cat( genome_tab$code[ !genome_tab$code %in% desired_order] )
+}
+
+######################
+extra_header <- c(rep('-', 2), 'assembly', rep('-', 2), 'dnaPipeTE', rep('-', 2), 'BUSCO', rep('-', 3), 'GenomeScope', rep('-', 2) )
+asm_template <- matrix(ncol = length(extra_header))
+colnames(asm_template) <- extra_header
+header <- as.data.frame(asm_template)[FALSE, ]
+
+write.table(header, tab_file, quote = F, sep = '\t', row.names = F)
+write.table(genome_tab, tab_file, quote = F, sep = '\t', row.names = F, append = T)
