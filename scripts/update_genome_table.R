0779f05991964f1b4a5cbed79ebcb8d8daa59cd1
Kamil S Jaroň <376090@mail.muni.cz>
2018-07-11 11:16:20 +0200

    pulling some biology from google table to the stat table for easy matching

--- scripts/update_genome_table.R
+++ scripts/update_genome_table.R
@@ -2,6 +2,8 @@
 ### load data ###
 #################
 
+library('gsheet')
+
 # table to update
 tab_file <- 'tables/genome_table.tsv'
 genome_tab <- read.table(tab_file, header = T, stringsAsFactors = F, skip = 1, check.names = F)
@@ -156,19 +158,51 @@ for(genomescope_file in genomescope_files){
     genome_tab[row, c('haploid_length[M]', 'repeats', 'heterozygosity')] <- parse_genomescope_summary(genomescope_file)
 }
 
+#####################################################
+### add reproduction mode, ploidy and genome_size ###
+#####################################################
+
+literature_data <- read.csv(text = gsheet2text("https://docs.google.com/spreadsheets/d/1T4BHQxzMGMlWiNJ7G9OLPXzdNBCqMw0mSO7C_ggGEbc/edit?usp=sharing", format='csv'),
+                            stringsAsFactors = F, skip = 1, header = T, check.names = F)
+literature_data <- literature_data[,c(2, 7, 8, 11)]
+
+meio <- grepl('auto', literature_data[,'reproduction_mode']) | grepl('meiosis', literature_data[,'reproduction_mode'])
+mito <- grepl('apo', literature_data[,'reproduction_mode']) | grepl('mitosis', literature_data[,'reproduction_mode'])
+gupl <- grepl('dupl', literature_data[,'reproduction_mode'])
+
+# create three categories
+literature_data$reproduction_mode[meio] <- 'automixis' # involve meiosis
+literature_data$reproduction_mode[mito] <- 'apomixis' # wo mitosis
+literature_data$reproduction_mode[gupl] <- 'gamete_duplication' # gamete duplications
+literature_data$reproduction_mode[ ! (meio | mito | gupl) ] <- NA # unknown
+
+literature_data <- literature_data[literature_data$code %in% genome_tab$code,]
+
+genome_tab$ploidy <- NA
+genome_tab$reproduction_mode <- NA
+genome_tab$hybrid_origin <- NA
+
+row.names(genome_tab) <- genome_tab$code
+columns <- c('ploidy', 'reproduction_mode', 'hybrid_origin')
+genome_tab[literature_data$code, columns] <- literature_data[, columns]
+
 ######################
 ### Sort the table ###
 ######################
 
 # soring rows
+# lines
+#  vertebrates
+#  rotifers
+#  arthropods
+#  nematodes; TODO add 'Anan1' to end
+#  tardigrades
 desired_order <- c('Pfor1',
                    'Avag1', 'Aric1', 'Rmac1', 'Rmag1',
-                   'Lcla1', 'Tpre1', 'Obir1', 'Aruf1', 'Fcan1',
-                   'Pvir1', 'Dpul1', 'Dpul2', 'Dpul3',
-                   'Ps591', 'Dcor1', 'Dpac1', 'Minc1', 'Minc2', 'Minc3', 'Mjav1', 'Mjav2', 'Mare1', 'Mare2', 'Mflo1', 'Mflo2', 'Ment1',
+                   'Lcla1', 'Tpre1', 'Obir1', 'Aruf1', 'Fcan1', 'Dpul1', 'Dpul2', 'Dpul3', 'Pvir1',
+                   'Psam1', 'Dcor1', 'Dpac1', 'Ps591', 'Minc1', 'Minc2', 'Minc3', 'Mjav1', 'Mjav2', 'Mare1', 'Mare2', 'Mflo1', 'Mflo2', 'Ment1',
                    'Hduj1', 'Rvar1')
 if ( length(desired_order) == nrow(genome_tab) ){
-    row.names(genome_tab) <- genome_tab$code
     genome_tab <- genome_tab[desired_order, ]
 } else {
     cat('There are species in the table with unknown position by the hand-made-order in this script.\n')
@@ -177,7 +211,7 @@ if ( length(desired_order) == nrow(genome_tab) ){
 }
 
 # sorting columns
-genome_tab <- genome_tab[, c('code', 'species',
+genome_tab <- genome_tab[, c('code', 'species', 'reproduction_mode', 'hybrid_origin', 'ploidy',
                              'assembly_size[M]', 'number_of_scaffolds[k]', 'N50[k]',
                              'complete', 'fragmented', 'duplicated', 'missing',
                              'haploid_length[M]', 'heterozygosity', 'repeats',
