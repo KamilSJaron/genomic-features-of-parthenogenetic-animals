01982bfa1212813b9bb62883ec0883e192298940
Kamil S Jaroň <376090@mail.muni.cz>
2018-10-02 19:11:44 +0200

    smudgeplot snakemake

---
+++ scripts/generate_smudgeplot.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+JELLYFISH_DIR=$1
+OUTDIR=$2
+
+JF_FILE=$JELLYFISH_DIR/*jf
+HISTOGRAM=$JELLYFISH_DIR/*hist
+KMER=$(echo $HISTOGRAM | cut -f 2 -d '_' | sed 's/^k//' | sed 's/.hist//')
+
+mkdir $OUTDIR
+
+SP=$(echo $OUTDIR | cut -f 2 -d \/)
+
+if [[ $SP == "Lcla1" || $SP == "Tpre1" || $SP == "Aruf1" ]]; then
+    HOMOZYG="--homozygous"
+fi
+
+L=$(kmer_cov_cutoff.R $HISTOGRAM L)
+U=$(kmer_cov_cutoff.R $HISTOGRAM U)
+
+jellyfish dump -c -L $L -U $U $JELLYFISH_FILE | hetkmers.py -k $KMER -t 8 -o $OUTDIR/kmer_pairs
+
+smudgeplot.R -i $OUTDIR/kmer_pairs_coverages_2.tsv -o $OUTDIR/$SP -t $SP -L $L -k $KMER $HOMOZYG
