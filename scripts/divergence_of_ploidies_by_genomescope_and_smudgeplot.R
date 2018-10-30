3fbb86f4c89b743f1e1308618ad335a302f629ba
Kamil S Jaroň <376090@mail.muni.cz>
2018-10-30 14:43:03 +0100

    the first draft of comparison of divergence of haplotypes within tirploids of smudgeplot and genomescope

---
+++ scripts/divergence_of_ploidies_by_genomescope_and_smudgeplot.R
@@ -0,0 +1,28 @@
+tripliods_gs <- read.table('tables/triploid_heterozygosity.tsv', header = T)
+
+smudge_files <- paste0('data/', tripliods_gs$code, '/smudgeplot/', tripliods_gs$code, '_summary_table.tsv')
+
+read.smudgesummary <- function(file){
+    read.table(file, sep = '\t', skip = 1,
+               col.names = c('smudge', 'kmer_pairs', 'proportion', 'center_sum', 'center_frac'))
+}
+
+triploid_smudges <- lapply(smudge_files, read.smudgesummary)
+
+smudges_AAB <- sapply(triploid_smudges, function(x){ x[x$smudge == 'AAB', 'proportion'] })
+smudges_AB <- sapply(triploid_smudges, function(x){ x[x$smudge == 'AB', 'proportion'] })
+smudges_AB <- c(0,unlist(smudges_AB))
+
+gs_div_score <- tripliods_gs$AB / (tripliods_gs$AB + tripliods_gs$AAB)
+smudge_div_score <- smudges_AB / (smudges_AB + smudges_AAB)
+
+png('~/Desktop/triploid_haplotype_structure.png')
+
+par(mar = c(5,5,4,2))
+plot(smudge_div_score ~ gs_div_score, pch = 20,
+     xlab = 'GenomeScope \n ABC / (AAB + ABC)', ylab = 'smudgeplot \n AB / (AAB + ABC)')
+text(gs_div_score, smudge_div_score, as.character(tripliods_gs$code), pos = 1)
+
+title('Higher the value is, more equidistant the three haplotypes are')
+
+dev.off()
