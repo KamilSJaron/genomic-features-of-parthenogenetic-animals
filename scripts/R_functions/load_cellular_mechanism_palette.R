8630b88ec0e97a942182ed97755e9c2e67d46aec
Kamil S Jaroň <376090@mail.muni.cz>
2018-11-07 16:25:47 +0100

    small adjustments to plotting scripts

---
+++ scripts/R_functions/load_cellular_mechanism_palette.R
@@ -0,0 +1,8 @@
+load_cellular_mechanism_palette <- function(){
+    c(  rgb(0.9411, 0.8941, 0.2588), # yellow - gamete duplication
+        rgb(0.9019, 0.6235, 0     ), # orange - terminal fusion
+        rgb(0.8352, 0.3686, 0     ), # vermillion - central fusion
+        rgb(0.8,    0.4745, 0.6549), # pink - automixis unknown
+        rgb(0,      3/5,    2/5   ), # blue - apomixis
+        rgb(0,      0.4470, 0.6980)) # green - unknown
+}
