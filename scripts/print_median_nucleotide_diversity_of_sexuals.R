9cba25c7061152018e75017f9179b4f7c7b1c4d9
Kamil S Jaroň <376090@mail.muni.cz>
2018-12-06 16:12:43 +0100

    print nucleotide diversity of sexuals; not used in the end, but commited for archiving purposes

---
+++ scripts/print_median_nucleotide_diversity_of_sexuals.R
@@ -0,0 +1,16 @@
+nt_div <- read.table('tables/nt_diversity.tsv', header = T, stringsAsFactors = F, sep = '\t')
+
+get_population_mean <- function(entry){
+    mean(as.numeric(unlist(strsplit(entry, ';'))))
+}
+
+nt_div <- nt_div[nt_div[,3] %in% c("Arthropoda", "Chordata", "Porifera", "Nematoda", "Mollusca", "Echinodermata"),]
+nt_div <- as.vector(sapply(nt_div[,6], get_population_mean))
+# print median of species (where value of a species is a mean of population estimates)
+cat(round(median(nt_div), 2))
+cat("\n")
+
+# mean(sort(nt_div) < 0.03)
+# [1] 0.03587444
+# mean(sort(nt_div) < 0.53)
+# [1] 0.4125561
--- tables/README.md
+++ tables/README.md
@@ -4,3 +4,5 @@
 - [genome_table.tsv](genome_table.tsv) - a table of all estimated genomic properties
 - [tetraploid_heterozygosity.tsv](tetraploid_heterozygosity.tsv) - the estimates of the heterozygosity distribution in tetraploid genomes
 - [triploid_heterozygosity.tsv](triploid_heterozygosity.tsv) - the estimates of the heterozygosity distribution in triploid genomes
+- [nt_diversity_extracted.tsv](nt_diversity_extracted.tsv) - the nucleotide diversities extracted from the supplementary table 1 of  https://doi.org/10.1371/journal.pbio.1001388
+
